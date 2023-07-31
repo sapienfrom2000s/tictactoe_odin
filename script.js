b7abd866704abd012ca07da674e747672726998b
brickwall <sk292207@gmail.com>
2023-07-31 21:50:52 +0530

    Add: board for display

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let Gameboard = function(){	
-  gameboard = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+const Gameboard = function(){	
+  const gameboard = [nil, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
   return {gameboard}
 }()
 
+const Players = function(){
+  const player1 = { name: 'Joe', symbol: 'X' }
+  const player2 = { name: 'Jean', symbol: '0'}
+  return { player1, player2 }
+}()
+
+function addgridtodocument(){
+  board = document.querySelector(".board") 
+  for(var i = 1; i <= 9; i++){
+    var new_div = document.createElement('div')
+    new_div.classList.add(`grid_${i}`)
+    board.appendChild(new_div) 
+  }
+}
