07ebffeef56fe39919fd1ddd42da76fe6304ac40
brickwall <sk292207@gmail.com>
2023-08-03 00:33:33 +0530

    Add logic for win and draw

--- script.js
+++ script.js
@@ -9,22 +9,79 @@ const Players = (function(){
   return { player1, player2 }
 })();
 
+const Turn = (function(){
+  let number = 1
+  return { number }
+})();
+
 const Active_Player = (function(){
-  let turn_number = 0
   return function(){
-    turn_number++
-    return ( turn_number % 2 == 0 ? Players.player2 :  Players.player1 )
+    return ( Turn.number % 2 == 0 ? Players.player2 :  Players.player1 )
   }
 })();
 
 (function enablegridinput(){
   var grid = document.querySelectorAll('.grid-container > *')
   grid.forEach(item => {
-    item.addEventListener('click', event => {
-      const active_player = Active_Player()
-      event.target.innerHTML = active_player.symbol
-      Gameboard.data[event.target.dataset.gridIndex] = active_player.symbol
-      console.log(Gameboard.data)
-    }, { once:true })
-  })})();
+    item.addEventListener('click', main, { once:true })
+})})();
 
+function disablegridinput(){
+  var grid = document.querySelectorAll('.grid-container > *')
+  grid.forEach(item => {
+    item.removeEventListener('click', main, { once:true })
+})}
+
+function main(event){
+  input(event)
+  // debugger
+  isGameover()
+  Turn.number++
+}
+
+function isGameover(){
+  if(isWinning(Active_Player().symbol)){
+    disablegridinput()
+    console.log('won')
+    //modal pop up
+  }
+  else if(isDraw()){
+    console.log('Draw')
+    //just check if Turn is 9th
+    //modal pop up
+  }
+}
+
+function isDraw(){
+  return Turn.number == 9 ? true : false
+}
+
+function input(event){
+  const active_player = Active_Player()
+  event.target.innerHTML = active_player.symbol
+  Gameboard.data[event.target.dataset.gridIndex] = active_player.symbol
+}
+
+function isWinning(symbol){
+  return (isHorizontal() || isVertical() || isDiagonal())
+  // hardcoded as there are less cases to cover
+  function isHorizontal(){
+    let board = Gameboard.data
+    return ((board[1] == board[2] && board[2] == board[3] && board[3] == symbol) ||
+      (board[4] == board[5] && board[5] == board[6] && board[6] == symbol) ||
+      (board[7] == board[8] && board[8] == board[9] && board[9] == symbol))
+  }
+
+  function isVertical(){
+    let board = Gameboard.data
+    return ((board[1] == board[4] && board[4] == board[7] && board[7] == symbol) ||
+      (board[2] == board[5] && board[5] == board[8] && board[8] == symbol) ||
+      (board[3] == board[6] && board[6] == board[9] && board[9] == symbol))
+  }
+
+  function isDiagonal(){
+    let board = Gameboard.data
+    return ((board[1] == board[5] && board[5] == board[9] && board[9] == symbol) ||
+     (board[3] == board[5] && board[5] == board[7] && board[7] == symbol))
+  }
+}
